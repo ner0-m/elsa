1d2d01e7d70ff9095329b8fd1801d33e3620c228
David <frankd@in.tum.de>
2019-12-15 02:50:25 +0100

    CMakes can find components dependencies on a find_package(elsa COMPONENTS ...) call

--- cmake/InstallElsaModule.cmake
+++ cmake/InstallElsaModule.cmake
@@ -1,11 +1,27 @@
 # install an elsa module
 
-function(InstallElsaModule ELSA_MODULE_NAME ELSA_MODULE_TARGET_NAME ELSA_MODULE_EXPORT_TARGET)
+function(InstallElsaModule ELSA_MODULE_NAME ELSA_MODULE_TARGET_NAME ELSA_MODULE_EXPORT_TARGET ELSA_COMPONENT_DEPENDENCIES)
     if(ELSA_INSTALL)
         # This is required so that the exported target has the name core and not elsa_core
         set_target_properties(${ELSA_MODULE_TARGET_NAME} PROPERTIES EXPORT_NAME ${ELSA_MODULE_NAME})
 
         include(GNUInstallDirs)
+        include(CMakePackageConfigHelpers)
+
+        #Create config file for each component
+        configure_package_config_file(
+            ${PROJECT_SOURCE_DIR}/cmake/elsaComponentConfig.cmake.in 
+            ${PROJECT_BINARY_DIR}/elsa/elsa_${ELSA_MODULE_NAME}Config.cmake 
+            INSTALL_DESTINATION ${INSTALL_CONFIG_DIR}
+        )
+
+        #and install it to lib/cmake/
+        install(FILES 
+            ${PROJECT_BINARY_DIR}/elsa/elsa_${ELSA_MODULE_NAME}Config.cmake 
+            DESTINATION ${INSTALL_CONFIG_DIR}
+        )
+
+
         # install the module
         install(TARGETS ${ELSA_MODULE_TARGET_NAME}
                 EXPORT ${ELSA_MODULE_EXPORT_TARGET}
