e75b58ef72b7b491c8f35781fe9c5818e11c32cc
David Tellenbach <david.tellenbach@tum.de>
2020-01-07 01:29:37 +0100

    [Tests] Add SoftmaxLayer to generic layer tests

--- elsa/ml/tests/test_BackendLayerTypes.cpp
+++ elsa/ml/tests/test_BackendLayerTypes.cpp
@@ -6,6 +6,7 @@
 #include "ConvLayer.h"
 #include "DenseLayer.h"
 #include "PoolingLayer.h"
+#include "SoftmaxLayer.h"
 
 using namespace elsa;
 
@@ -15,13 +16,12 @@ TEMPLATE_TEST_CASE("BackendLayerTypes", "elsa_ml", float)
     {
         constexpr MlBackend Backend = MlBackend::Dnnl;
 
-        REQUIRE(
-            std::is_same_v<typename Relu<TestType, Backend>::BackendLayerType, DnnlRelu<TestType>>);
-        REQUIRE(std::is_same_v<typename ConvLayer<TestType, Backend>::BackendLayerType,
-                               DnnlConvLayer<TestType>>);
-        REQUIRE(std::is_same_v<typename DenseLayer<TestType, Backend>::BackendLayerType,
-                               DnnlDenseLayer<TestType>>);
-        REQUIRE(std::is_same_v<typename PoolingLayer<TestType, Backend>::BackendLayerType,
-                               DnnlPoolingLayer<TestType>>);
+        // clang-format off
+        REQUIRE(std::is_same_v<typename Relu         <TestType, Backend>::BackendLayerType, DnnlRelu<TestType>>);
+        REQUIRE(std::is_same_v<typename ConvLayer    <TestType, Backend>::BackendLayerType, DnnlConvLayer<TestType>>);
+        REQUIRE(std::is_same_v<typename DenseLayer   <TestType, Backend>::BackendLayerType, DnnlDenseLayer<TestType>>);
+        REQUIRE(std::is_same_v<typename PoolingLayer <TestType, Backend>::BackendLayerType, DnnlPoolingLayer<TestType>>);
+        REQUIRE(std::is_same_v<typename SoftmaxLayer <TestType, Backend>::BackendLayerType, DnnlSoftmaxLayer<TestType>>);
+        // clang-format on
     }
 }
