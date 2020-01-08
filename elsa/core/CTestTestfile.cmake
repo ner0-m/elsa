088e4b600d8bd563763ebf410fe6e04c4817c68f
Rebecca <rebecca.richter@tum.de>
2020-01-08 17:12:22 +0100

    Added some functionality to the Logger, replaced thirdparty::spdlog library by newer version

--- elsa/core/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/beccy/Dokumente/Master BMC/2.Semester/BAReloaded/Local Libraries/elsa/elsa/core
-# Build directory: /home/beccy/Dokumente/Master BMC/2.Semester/BAReloaded/Local Libraries/elsa/elsa/core
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
