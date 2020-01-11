631637d625ea6ea4f5d2db6c848ecadc9a521da9
David Tellenbach <david.tellenbach@tum.de>
2020-01-11 16:08:07 +0100

    Refactor activation and dense backend layer tests

--- elsa/ml/tests/test_Activation.cpp
+++ elsa/ml/tests/test_Activation.cpp
@@ -1,5 +1,5 @@
 #include <catch2/catch.hpp>
-#include <type_traits>
+#include <iostream>
 #include <random>
 
 #include "elsaDefines.h"
@@ -12,21 +12,23 @@ TEST_CASE("Relu semantics", "elsa_ml")
 {
     using Layer = Relu<float, MlBackend::Dnnl>;
 
+    std::mt19937 mt(123); // The random number generator using a deterministic seed
+    std::uniform_real_distribution<float> dist(-1, 1);
+
     IndexVector_t inputVec(4);
     inputVec << 1, 1, 4, 4;
     DataDescriptor inputDesc(inputVec);
 
     Layer layer(inputDesc);
+    float alpha = 0.5f;
+    layer.setAlpha(alpha);
 
     SECTION("Forward semantics")
     {
-        std::mt19937 mt(123); // The random number generator using a deterministic seed
-        std::uniform_real_distribution<float> dist;
-
         DataContainer<float> input(inputDesc);
 
-        for (int i = 0; i < 16; ++i)
-            input[i] = dist(mt);
+        for (auto& coeff : input)
+            coeff = dist(mt);
 
         auto backend = layer.getBackend();
         backend->setInput(input);
@@ -37,28 +39,28 @@ TEST_CASE("Relu semantics", "elsa_ml")
         s.wait();
         auto output = backend->getOutput();
 
-        for (int i = 0; i < 16; ++i)
-            REQUIRE(output[i] == (input[i] < 0 ? Approx(0.0f) : Approx(input[i])));
+        auto f = [&alpha](const auto& coeff) {
+            if (coeff <= 0)
+                return alpha * coeff;
+            else
+                return coeff;
+        };
+
+        for (int i = 0; i < output.getDataDescriptor().getNumberOfCoefficients(); ++i)
+            REQUIRE(output[i] == Approx(f(input[i])).epsilon(0.001));
     }
 
     SECTION("Backward semantics")
     {
-        std::mt19937 mt(123); // The random number generator using a deterministic seed
-        std::uniform_real_distribution<float> dist;
-
         DataContainer<float> input(inputDesc);
         DataContainer<float> outputGradient(inputDesc);
 
-        for (int i = 0; i < 16; ++i) {
-            input[i] = dist(mt);
-            outputGradient = dist(mt);
-        }
+        for (auto& coeff : outputGradient)
+            coeff = dist(mt);
 
         auto backend = layer.getBackend();
         backend->setInput(input);
-        backend->setOutputGradient(input);
-        std::static_pointer_cast<typename decltype(layer)::BackendLayerType>(backend)->setAlpha(
-            .5f);
+        backend->setOutputGradient(outputGradient);
         backend->compile(PropagationKind::Full);
         auto engine = backend->getEngine();
         dnnl::stream s(*engine);
@@ -66,9 +68,15 @@ TEST_CASE("Relu semantics", "elsa_ml")
         s.wait();
         auto inputGradient = backend->getInputGradient();
 
-        for (int i = 0; i < 16; ++i)
-            REQUIRE(inputGradient[i]
-                    == (outputGradient[i] < 0 ? Approx(.5f * input[i]) : Approx(input[i])));
+        auto f_der = [&alpha](const auto& coeff) {
+            if (coeff <= 0)
+                return alpha;
+            else
+                return 1.f;
+        };
+
+        for (int i = 0; i < inputGradient.getDataDescriptor().getNumberOfCoefficients(); ++i)
+            REQUIRE(inputGradient[i] == Approx(f_der(input[i]) * outputGradient[i]).epsilon(0.001));
     }
 }
 
@@ -76,6 +84,9 @@ TEST_CASE("Abs semantics", "elsa_ml")
 {
     using Layer = Abs<float, MlBackend::Dnnl>;
 
+    std::mt19937 mt(123); // The random number generator using a deterministic seed
+    std::uniform_real_distribution<float> dist(-1, 1);
+
     IndexVector_t inputVec(4);
     inputVec << 1, 1, 4, 4;
     DataDescriptor inputDesc(inputVec);
@@ -84,13 +95,10 @@ TEST_CASE("Abs semantics", "elsa_ml")
 
     SECTION("Forward semantics")
     {
-        std::mt19937 mt(123); // The random number generator using a deterministic seed
-        std::uniform_real_distribution<float> dist;
-
         DataContainer<float> input(inputDesc);
 
-        for (int i = 0; i < 16; ++i)
-            input[i] = dist(mt);
+        for (auto& coeff : input)
+            coeff = dist(mt);
 
         auto backend = layer.getBackend();
         backend->setInput(input);
@@ -101,25 +109,23 @@ TEST_CASE("Abs semantics", "elsa_ml")
         s.wait();
         auto output = backend->getOutput();
 
-        for (int i = 0; i < 16; ++i)
-            REQUIRE(output[i] == std::abs(input[i]));
+        auto f = [](const auto& coeff) { return std::abs(coeff); };
+
+        for (int i = 0; i < output.getDataDescriptor().getNumberOfCoefficients(); ++i)
+            REQUIRE(output[i] == Approx(f(input[i])).epsilon(0.001));
     }
+
     SECTION("Backward semantics")
     {
-        std::mt19937 mt(123); // The random number generator using a deterministic seed
-        std::uniform_real_distribution<float> dist;
-
         DataContainer<float> input(inputDesc);
         DataContainer<float> outputGradient(inputDesc);
 
-        for (int i = 0; i < 16; ++i) {
-            input[i] = dist(mt);
-            outputGradient = dist(mt);
-        }
+        for (auto& coeff : outputGradient)
+            coeff = dist(mt);
 
         auto backend = layer.getBackend();
         backend->setInput(input);
-        backend->setOutputGradient(input);
+        backend->setOutputGradient(outputGradient);
         backend->compile(PropagationKind::Full);
         auto engine = backend->getEngine();
         dnnl::stream s(*engine);
@@ -127,9 +133,17 @@ TEST_CASE("Abs semantics", "elsa_ml")
         s.wait();
         auto inputGradient = backend->getInputGradient();
 
-        for (int i = 0; i < 16; ++i)
-            REQUIRE(inputGradient[i]
-                    == (outputGradient[i] < 0 ? Approx(-1.f * input[i]) : Approx(input[i])));
+        auto f_der = [](const auto& coeff) {
+            if (coeff == 0)
+                return 0.f;
+            else if (coeff < 0)
+                return -1.f;
+            else
+                return 1.f;
+        };
+
+        for (int i = 0; i < inputGradient.getDataDescriptor().getNumberOfCoefficients(); ++i)
+            REQUIRE(inputGradient[i] == Approx(f_der(input[i]) * outputGradient[i]).epsilon(0.001));
     }
 }
 
@@ -137,23 +151,24 @@ TEST_CASE("Elu semantics", "elsa_ml")
 {
     using Layer = Elu<float, MlBackend::Dnnl>;
 
+    std::mt19937 mt(123); // The random number generator using a deterministic seed
+    std::uniform_real_distribution<float> dist(-1, 1);
+
     IndexVector_t inputVec(4);
-    inputVec << 3, 2, 4, 4;
+    inputVec << 1, 1, 4, 4;
     DataDescriptor inputDesc(inputVec);
 
-    std::mt19937 mt(123); // The random number generator using a deterministic seed
-    std::uniform_real_distribution<float> dist;
-
-    DataContainer<float> input(inputDesc);
-
-    for (auto& coeff : input)
-        coeff = dist(mt);
-
     Layer layer(inputDesc);
+    float alpha = dist(mt);
+    layer.setAlpha(alpha);
 
     SECTION("Forward semantics")
     {
-        layer.setAlpha(2.f);
+        DataContainer<float> input(inputDesc);
+
+        for (auto& coeff : input)
+            coeff = dist(mt);
+
         auto backend = layer.getBackend();
         backend->setInput(input);
         backend->compile();
@@ -163,27 +178,28 @@ TEST_CASE("Elu semantics", "elsa_ml")
         s.wait();
         auto output = backend->getOutput();
 
-        for (int i = 0; i < inputDesc.getNumberOfCoefficients(); ++i)
-            REQUIRE(output[i]
-                    == (input[i] < 0 ? Approx(2.f * (std::exp(input[i]) - 1)) : Approx(input[i])));
+        auto f = [&alpha](const auto& coeff) {
+            if (coeff <= 0)
+                return alpha * (std::exp(coeff) - 1);
+            else
+                return coeff;
+        };
+
+        for (int i = 0; i < output.getDataDescriptor().getNumberOfCoefficients(); ++i)
+            REQUIRE(output[i] == Approx(f(input[i])).epsilon(0.001));
     }
 
     SECTION("Backward semantics")
     {
-        std::mt19937 mt(123); // The random number generator using a deterministic seed
-        std::uniform_real_distribution<float> dist;
-
         DataContainer<float> input(inputDesc);
         DataContainer<float> outputGradient(inputDesc);
 
-        for (int i = 0; i < 16; ++i) {
-            input[i] = dist(mt);
-            outputGradient = dist(mt);
-        }
+        for (auto& coeff : outputGradient)
+            coeff = dist(mt);
 
         auto backend = layer.getBackend();
         backend->setInput(input);
-        backend->setOutputGradient(input);
+        backend->setOutputGradient(outputGradient);
         backend->compile(PropagationKind::Full);
         auto engine = backend->getEngine();
         dnnl::stream s(*engine);
@@ -191,9 +207,78 @@ TEST_CASE("Elu semantics", "elsa_ml")
         s.wait();
         auto inputGradient = backend->getInputGradient();
 
-        for (int i = 0; i < 16; ++i)
-            REQUIRE(inputGradient[i]
-                    == (outputGradient[i] < 0 ? Approx(2.f * std::exp(input[i]) * input[i])
-                                              : Approx(input[i])));
+        auto f_der = [&alpha](const auto& coeff) {
+            if (coeff <= 0)
+                return alpha * std::exp(coeff);
+            else
+                return 1.f;
+        };
+
+        for (int i = 0; i < inputGradient.getDataDescriptor().getNumberOfCoefficients(); ++i)
+            REQUIRE(inputGradient[i] == Approx(f_der(input[i]) * outputGradient[i]).epsilon(0.001));
+    }
+}
+
+TEST_CASE("Linear semantics", "elsa_ml")
+{
+    using Layer = Linear<float, MlBackend::Dnnl>;
+
+    std::mt19937 mt(123); // The random number generator using a deterministic seed
+    std::uniform_real_distribution<float> dist(-1, 1);
+
+    IndexVector_t inputVec(4);
+    inputVec << 1, 1, 4, 4;
+    DataDescriptor inputDesc(inputVec);
+
+    Layer layer(inputDesc);
+    float alpha = dist(mt);
+    float beta = dist(mt);
+    layer.setAlpha(alpha);
+    layer.setBeta(beta);
+
+    SECTION("Forward semantics")
+    {
+        DataContainer<float> input(inputDesc);
+
+        for (auto& coeff : input)
+            coeff = dist(mt);
+
+        auto backend = layer.getBackend();
+        backend->setInput(input);
+        backend->compile();
+        auto engine = backend->getEngine();
+        dnnl::stream s(*engine);
+        backend->forwardPropagate(s);
+        s.wait();
+        auto output = backend->getOutput();
+
+        auto f = [&alpha, &beta](const auto& coeff) { return alpha * coeff + beta; };
+
+        for (int i = 0; i < output.getDataDescriptor().getNumberOfCoefficients(); ++i)
+            REQUIRE(output[i] == Approx(f(input[i])).epsilon(0.001));
+    }
+
+    SECTION("Backward semantics")
+    {
+        DataContainer<float> input(inputDesc);
+        DataContainer<float> outputGradient(inputDesc);
+
+        for (auto& coeff : outputGradient)
+            coeff = dist(mt);
+
+        auto backend = layer.getBackend();
+        backend->setInput(input);
+        backend->setOutputGradient(outputGradient);
+        backend->compile(PropagationKind::Full);
+        auto engine = backend->getEngine();
+        dnnl::stream s(*engine);
+        backend->backwardPropagate(s);
+        s.wait();
+        auto inputGradient = backend->getInputGradient();
+
+        auto f_der = [&alpha](const auto& coeff) { return alpha; };
+
+        for (int i = 0; i < inputGradient.getDataDescriptor().getNumberOfCoefficients(); ++i)
+            REQUIRE(inputGradient[i] == Approx(f_der(input[i]) * outputGradient[i]).epsilon(0.001));
     }
 }
--- elsa/ml/tests/test_DenseLayer.cpp
+++ elsa/ml/tests/test_DenseLayer.cpp
@@ -1,5 +1,5 @@
 #include <catch2/catch.hpp>
-#include <type_traits>
+#include <iostream>
 #include <random>
 
 #include "elsaDefines.h"
@@ -8,56 +8,127 @@
 
 using namespace elsa;
 
+struct DenseReference {
+    static Eigen::VectorXf forwardPropagate(const Eigen::VectorXf& input,
+                                            const Eigen::MatrixXf& weights,
+                                            const Eigen::VectorXf& bias)
+    {
+        return weights * input + bias;
+    }
+
+    static Eigen::VectorXf backwardPropagate(const Eigen::VectorXf& input,
+                                             const Eigen::MatrixXf& weights,
+                                             Eigen::VectorXf& outputGradient)
+    {
+        Eigen::VectorXf ret;
+        ret.resizeLike(input);
+
+        for (int i = 0; i < ret.size(); ++i)
+            ret(i) = weights.col(i).dot(outputGradient);
+
+        return ret;
+    }
+};
+
 TEST_CASE("DenseLayer semantics")
 {
-    IndexVector_t inputVec(2);
-    // One batch dimension and input size is 5
-    inputVec << 2, 5;
-    DataDescriptor inputDesc(inputVec);
-    Eigen::VectorXf inputValues(2 * 5);
-    inputValues << 1, 2, 1, 1, 5, 1, 2, 1, 1, 5;
-    DataContainer<float> input(inputDesc, inputValues);
+    SECTION("Forward Test 1")
+    {
+        IndexVector_t inputVec(2);
+        // One batch dimension and input size is 5
+        inputVec << 2, 5;
+        DataDescriptor inputDesc(inputVec);
+        Eigen::VectorXf inputValues(2 * 5);
+        inputValues << 1, 2, 1, 1, 5, 1, 2, 1, 1, 5;
+        DataContainer<float> input(inputDesc, inputValues);
 
-    IndexVector_t weightsVec(2);
-    // Three neurons and input size is 5
-    weightsVec << 3, 5;
-    DataDescriptor weightsDesc(weightsVec);
-    Eigen::VectorXf weightsValues(3 * 5);
-    // clang-format off
+        IndexVector_t weightsVec(2);
+        // Three neurons and input size is 5
+        weightsVec << 3, 5;
+        DataDescriptor weightsDesc(weightsVec);
+        Eigen::VectorXf weightsValues(3 * 5);
+        // clang-format off
     weightsValues <<  1,  2,  3,  4,  5,
                      -1, -2, -3, -4, -5,
                       5,  4,  3,  2,  1;
-    // clang-format on
-    DataContainer<float> weights(weightsDesc, weightsValues);
+        // clang-format on
+        DataContainer<float> weights(weightsDesc, weightsValues);
 
-    IndexVector_t biasVec(1);
-    biasVec << 3;
-    DataDescriptor biasDesc(biasVec);
-    Eigen::VectorXf biasValues(1 * 3);
-    biasValues << 1, 0, 1;
-    DataContainer<float> bias(biasDesc, biasValues);
+        IndexVector_t biasVec(1);
+        biasVec << 3;
+        DataDescriptor biasDesc(biasVec);
+        Eigen::VectorXf biasValues(1 * 3);
+        biasValues << 1, 0, 1;
+        DataContainer<float> bias(biasDesc, biasValues);
 
-    DenseLayer<float> dense(inputDesc, 3);
+        DenseLayer<float> dense(inputDesc, 3);
 
-    auto backend = dense.getBackend();
-    backend->setInput(input);
-    backend->compile();
-    std::static_pointer_cast<typename DenseLayer<float>::BackendLayerType>(backend)->setWeights(
-        weights);
-    std::static_pointer_cast<typename DenseLayer<float>::BackendLayerType>(backend)->setBias(bias);
+        auto backend = dense.getBackend();
+        backend->setInput(input);
+        backend->compile();
+        std::static_pointer_cast<typename DenseLayer<float>::BackendLayerType>(backend)->setWeights(
+            weights);
+        std::static_pointer_cast<typename DenseLayer<float>::BackendLayerType>(backend)->setBias(
+            bias);
 
-    auto engine = backend->getEngine();
-    dnnl::stream s(*engine);
-    backend->forwardPropagate(s);
-    auto output = backend->getOutput();
+        auto engine = backend->getEngine();
+        dnnl::stream s(*engine);
+        backend->forwardPropagate(s);
+        auto output = backend->getOutput();
 
-    // Eigen::VectorXf required(2 * 3);
-    // required << 38, -37, 24, 38, -37, 24;
+        Eigen::VectorXf required(2 * 3);
+        required << 38, -37, 24, 38, -37, 24;
 
-    // // We expect one batch and one spatial dimension
-    // REQUIRE(output.getDataDescriptor().getNumberOfDimensions() == 2);
-    // REQUIRE(output.getDataDescriptor().getNumberOfCoefficients() == 2 * 3);
+        // We expect one batch and one spatial dimension
+        REQUIRE(output.getDataDescriptor().getNumberOfDimensions() == 2);
+        REQUIRE(output.getDataDescriptor().getNumberOfCoefficients() == 2 * 3);
 
-    // for (int i = 0; i < 2 * 3; ++i)
-    //     REQUIRE(output[i] == required[i]);
+        for (int i = 0; i < 2 * 3; ++i)
+            REQUIRE(output[i] == required[i]);
+    }
+
+    SECTION("Forward Test 2")
+    {
+        IndexVector_t inputVec(2);
+        inputVec << 1, 100;
+        DataDescriptor inputDesc(inputVec);
+        Eigen::VectorXf inputValues = Eigen::VectorXf::Random(100);
+        DataContainer<float> input(inputDesc, inputValues);
+
+        IndexVector_t biasVec(1);
+        biasVec << 64;
+        DataDescriptor biasDesc(biasVec);
+        Eigen::VectorXf biasValues = Eigen::VectorXf::Random(64);
+        DataContainer<float> bias(biasDesc, biasValues);
+
+        IndexVector_t weightsVec(2);
+        weightsVec << 64, 100;
+        DataDescriptor weightsDesc(weightsVec);
+        Eigen::MatrixXf weightsMat = Eigen::MatrixXf::Random(64, 100);
+        auto required = DenseReference::forwardPropagate(inputValues, weightsMat, biasValues);
+        weightsMat.transposeInPlace();
+        Eigen::VectorXf weightsValues(Eigen::Map<Eigen::VectorXf>(weightsMat.data(), 64 * 100));
+
+        DataContainer<float> weights(weightsDesc, weightsValues);
+
+        DenseLayer<float> dense(inputDesc, 64);
+
+        auto backend = dense.getBackend();
+        backend->setInput(input);
+        backend->compile();
+        std::static_pointer_cast<typename DenseLayer<float>::BackendLayerType>(backend)->setWeights(
+            weights);
+        std::static_pointer_cast<typename DenseLayer<float>::BackendLayerType>(backend)->setBias(
+            bias);
+
+        auto engine = backend->getEngine();
+        dnnl::stream s(*engine);
+        backend->forwardPropagate(s);
+        auto output = backend->getOutput();
+
+        REQUIRE(output.getDataDescriptor() == dense.getOutputDescriptor());
+
+        for (int i = 0; i < output.getDataDescriptor().getNumberOfCoefficients(); ++i)
+            REQUIRE(required(i) == Approx(output[i]));
+    }
 }
