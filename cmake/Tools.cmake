399861b86529a2e9b087eca34264077441eb1a78
David Frank <frankd@in.tum.de>
2020-03-27 14:56:25 +0100

    Add clang-format target to CMake, integrate clang-tidy in CMake
    format-cxx is formats the codebase according to the .clang-tidy file
    
    clang-tidy is now used in CMake, with the compile options ELSA_CLANG_TIDY, it can be turned on. Then building elsa will show the clang-tidy checks as well. CI pipeline uses this target with a new build-tests target, which only builds the tests and does not run it. The job exports a clang-tidy-fix.txt file, which shows possible fixes for the stuff clang-tidy found

--- cmake/Tools.cmake
+++ cmake/Tools.cmake
@@ -2,12 +2,113 @@
 #   original copyright by George Cave - gcave@stablecoder.ca
 #   original license Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
 
+# Helper append function
 function(append value)
   foreach(variable ${ARGN})
     set(${variable}
         "${${variable}} ${value}"
         PARENT_SCOPE)
   endforeach(variable)
+endfunction()
+
+#
+# clang-tidy
+#
+
+# Adds clang-tidy checks to the compilation, with the given arguments being used
+# as the options set.
+macro(clang_tidy)
+  if(ELSA_CLANG_TIDY AND CLANG_TIDY_EXE)
+    set(CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY_EXE} ${ARGN})
+  endif()
+endmacro()
+
+# Find clang-tidy
+find_program(CLANG_TIDY_EXE NAMES "clang-tidy-8")
+mark_as_advanced(FORCE CLANG_TIDY_EXE)
+if(CLANG_TIDY_EXE)
+  message(STATUS "clang-tidy-8 found: ${CLANG_TIDY_EXE}")
+  if(NOT ELSA_CLANG_TIDY)
+    message(STATUS "clang-tidy-8 NOT ENABLED via 'ELSA_CLANG_TIDY' variable!")
+    set(CMAKE_CXX_CLANG_TIDY
+            ""
+            CACHE STRING "" FORCE) # delete it
+  endif()
+elseif(ELSA_CLANG_TIDY)
+  message(SEND_ERROR "Cannot enable clang-tidy-8, as executable not found!")
+  set(CMAKE_CXX_CLANG_TIDY
+          ""
+          CACHE STRING "" FORCE) # delete it
+else()
+  message(STATUS "clang-tidy-8 not found!")
+  set(CMAKE_CXX_CLANG_TIDY
+          ""
+          CACHE STRING "" FORCE) # delete it
+endif()
+
+#
+# clang-format
+#
+find_program(CLANG_FORMAT_EXE "clang-format-8")
+mark_as_advanced(FORCE CLANG_FORMAT_EXE)
+if(CLANG_FORMAT_EXE)
+  message(STATUS "clang-format-8 found: ${CLANG_FORMAT_EXE}")
+else()
+  message(STATUS "clang-format-8 not found!")
+endif()
+
+# Generates a 'format' target using a custom name, files, and include
+# directories all being parameters.
+#
+# Do note that in order for sources to be inherited properly, the source paths
+# must be reachable from where the macro is called, or otherwise require a full
+# path for proper inheritance.
+#
+# ~~~
+# Required:
+# TARGET_NAME - The name of the target to create.
+#
+# Optional: ARGN - The list of targets OR files to format. Relative and absolute
+# paths are accepted.
+# ~~~
+function(clang_format TARGET_NAME)
+  if(CLANG_FORMAT_EXE)
+    set(FORMAT_FILES)
+    # Check through the ARGN's, determine existent files
+    foreach(item IN LISTS ARGN)
+      if(TARGET ${item})
+        # If the item is a target, then we'll attempt to grab the associated
+        # source files from it.
+        get_target_property(_TARGET_TYPE ${item} TYPE)
+        if(NOT _TARGET_TYPE STREQUAL "INTERFACE_LIBRARY")
+          get_property(
+            _TEMP
+            TARGET ${item}
+            PROPERTY SOURCES)
+          foreach(iter IN LISTS _TEMP)
+            if(EXISTS ${iter})
+              set(FORMAT_FILES ${FORMAT_FILES} ${iter})
+            endif()
+          endforeach()
+        endif()
+      elseif(EXISTS ${item})
+        # Check if it's a full file path
+        set(FORMAT_FILES ${FORMAT_FILES} ${item})
+      elseif(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${item})
+        # Check if it's based on the current source dir
+        set(FORMAT_FILES ${FORMAT_FILES} ${CMAKE_CURRENT_SOURCE_DIR}/${item})
+      endif()
+    endforeach()
+
+    # Make the target
+    if(FORMAT_FILES)
+      if(NOT TARGET ${TARGET_NAME})
+        add_custom_target(${TARGET_NAME} COMMAND ${CLANG_FORMAT_EXE} -i
+                                                 -style=file ${FORMAT_FILES})
+      endif()
+    endif()
+
+  endif()
 endfunction()
 
 if(ELSA_SANITIZER)
