1cd68affe5340f2e1f3d1e5a1d8d4edaf8eec622
David Frank <frankd@in.tum.de>
2021-05-18 10:30:24 +0200

    Use CMAKE_CUDA_ARCHITECTURES to determine compute capabilities
    In CMake 3.18 the (CMAKE_)CUDA_ARCHITECTURES variables were introduced.
    CMake uses them to pass the compute capabilities to the CUDA compiler.
    
    Therefore, for CMake versions 3.18 and higher, we rely on this CMake
    behaviour and while we still support previous versions, the previous
    behaviour is still kept around.
    
    CMAKE_CUDA_ARCHITECTURES is set to 'Auto' by default, which is
    equivalent to the previous CUDA_ARCH variables that was used.
    Overwriting via the command line is possible for cross compilation of
    CUDA code.

--- cmake/ComputeCapabilityCUDA.cmake
+++ cmake/ComputeCapabilityCUDA.cmake
@@ -1,5 +1,27 @@
-cmake_minimum_required(VERSION 3.10)
-
+ 
+################################################################################################
+# Check if elsa_arch_type is set to auto, if yes, then the current CUDA capabilities are
+# checked added to 
+function(set_cuda_arch_type elsa_arch_type)
+    string( TOLOWER "${elsa_arch_type}" _arch_type) 
+     
+    if (${_arch_type} STREQUAL "auto")
+        cuda_detect_installed_gpus(_detected_capabilities)
+        string(REPLACE "." "" _target_gpus ${_detected_capabilities}) 
+    else()
+        set(_target_gpus ${elsa_arch_type})
+    endif() 
+     
+    # TODO: Remove me once we upgrade to version 3.18
+    if(${CMAKE_VERSION} VERSION_GREATER_EQUAL 3.18) 
+        set(CMAKE_CUDA_ARCHITECTURES ${_target_gpus} PARENT_SCOPE) 
+    else() 
+        foreach(target_gpu ${_target_gpus})
+          string(REPLACE "." "" _target_capability ${target_gpu})
+          set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} --generate-code arch=compute_${_target_capability},code=[sm_${TARGET_GPU},compute_${TARGET_GPU}]")
+        endforeach()
+    endif() 
+endfunction() 
 ################################################################################################
 # Automatic GPU detection is not included with every CMake release (e.g. it is unavailable in
 # the CMake version installed with the default Ubuntu package manager)
