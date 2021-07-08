bc3196371a0de8649b5124547790c035f5e44892
Buerger <jonas.buerger@tum.de>
2021-07-08 16:43:04 +0200

    Complete implementation of image denoising python skript

--- examples/image_denoising.py
+++ examples/image_denoising.py
@@ -33,6 +33,7 @@ def im2patches(image, blocksize, stride):
 
 def patches2im(patches, imageshape, blocksize, stride):
     image = np.zeros(imageshape)
+    coeffs = np.zeros(imageshape) # keep track of how many patches have been used to create a pixel
     last_patch_x = math.floor(imageshape[0] - blocksize)
     last_patch_y = math.floor(imageshape[1] - blocksize)
 
@@ -41,60 +42,76 @@ def patches2im(patches, imageshape, blocksize, stride):
     for i in range(patches.shape[0]):
         patch = patches[i,:].reshape(blocksize, blocksize, order='F')
         image[x:x+blocksize, y:y+blocksize] += patch
+        coeffs[x:x+blocksize, y:y+blocksize] += np.ones(patch.shape)
 
-        if(y < last_patch_y):
+        if(y + stride <= last_patch_y):
             y += stride
         else:
             y = 0
             x += stride
 
-    image /= (blocksize*blocksize/stride)
+    coeffs[coeffs == 0] = 1 # avoid dividing by zero for pixels that don't appear in any patch
+    image /= coeffs
 
     return image
 
-def plot_and_save(original, noisy, reconstruction):
+def save_results(original, noisy, reconstruction):
     filename = "imagedenoising"
     if not os.path.exists(filename):
         os.makedirs(filename)
     timestamp = datetime.now().strftime("%y%m%d_%H%M")
-    filename += "/" + timestamp + ".png"
+    filename += "/" + timestamp
 
-    plt.subplot(131)
-    plt.imshow(raccoon, cmap='gray')
-    plt.subplot(132)
-    plt.imshow(noisy_raccoon, cmap='gray')
-    plt.subplot(133)
-    plt.imshow(reconstructed_raccoon, cmap='gray')
-    plt.savefig(filename, dpi=300, bbox_inches='tight')
+    with open(filename + "_params.txt", 'w') as f:
+        f.write(f"Gaussian noise sigma: {sigma}\n")
+        f.write(f"Blocksize {blocksize}, Stride {stride}\n")
+        f.write(f"Number of atoms {nAtoms}, Sparsity level {sparsityLevel}\n")
+        f.write(f"KSVD iterations: {nIterations}\n")
+
+    plt.imsave(filename + "_original.png", original, cmap='gray')
+    plt.imsave(filename + "_noisy.png", noisy, cmap='gray')
+    plt.imsave(filename + "_reconstruction.png", reconstruction, cmap='gray')
     plt.show()
 
-#############MAIN######################
 
+############PARAMS#####################
+sigma = 0.05
+blocksize = 5
+stride = 4
+nAtoms = 128
+sparsityLevel = 6
+nIterations = 15
+#############MAIN######################
+elsa.Logger.setLevel(elsa.LogLevel.OFF)
+
+print("Image Denoising: Preprocessing image...")
 raccoon = scipy.misc.face(gray=True)
 raccoon = raccoon / 255.0 #scale between 0 and 1
-#downsample size by 4, magic oneliner I found on stackoverflow
-raccoon = raccoon[::4, ::4] + raccoon[1::4, ::4] + raccoon[::4, 1::4] + raccoon[1::4, 1::4]
-raccoon /= 4.0
+#downsample size by 2, magic oneliner I found on stackoverflow
+raccoon = raccoon[::2, ::2] + raccoon[1::2, ::2] + raccoon[::2, 1::2] + raccoon[1::2, 1::2]
+raccoon /= 2.0
 
-noise = np.random.normal(0,0.1,raccoon.shape)
+noise = np.random.normal(0,sigma,raccoon.shape)
 noisy_raccoon = raccoon + noise 
 
-patched_raccoon = im2patches(noisy_raccoon, 12, 4)
+patched_raccoon = im2patches(noisy_raccoon, blocksize, stride)
 n_patches = patched_raccoon.shape[0]
 patchlength = patched_raccoon.shape[1]
 patch_descriptor = elsa.pyelsa_core.VolumeDescriptor([patchlength])
 raccoon_descriptor = elsa.pyelsa_core.IdenticalBlocksDescriptor(n_patches, patch_descriptor)
 raccoon_dc = elsa.pyelsa_core.DataContainer(raccoon_descriptor, patched_raccoon.flatten())
 
-problem = elsa.pyelsa_problems.DictionaryLearningProblem(raccoon_dc, 128)
-solver = elsa.pyelsa_solvers.KSVD(problem, 16)
-representations = solver.solve(50)
+print("Image Denoising: Learning dictionary and representations...")
+problem = elsa.pyelsa_problems.DictionaryLearningProblem(raccoon_dc, nAtoms)
+solver = elsa.pyelsa_solvers.KSVD(problem, sparsityLevel)
+representations = solver.solve(nIterations)
 dictionary = solver.getLearnedDictionary()
 
 reconstructed_raccoon_patches = np.zeros(patched_raccoon.shape)
 for i in range(n_patches):
     reconstructed_raccoon_patches[i] = np.array(dictionary.apply(representations.getBlock(i)))
 
-reconstructed_raccoon = patches2im(reconstructed_raccoon_patches, raccoon.shape, 12, 4)
+print("Image Denoising: Reconstructing image from resulting patches")
+reconstructed_raccoon = patches2im(reconstructed_raccoon_patches, raccoon.shape, blocksize, stride)
 
-plot_and_save(raccoon, noisy_raccoon, reconstructed_raccoon)
+save_results(raccoon, noisy_raccoon, reconstructed_raccoon)
