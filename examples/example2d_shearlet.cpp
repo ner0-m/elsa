52af79db92c8671ebfd4d4b3c1ff2ffdd05aa4d5
andibraimllari <andibraimllari@gmail.com>
2021-09-26 18:53:22 +0200

    renaming in shearlet example

--- examples/example2d_shearlet.cpp
+++ examples/example2d_shearlet.cpp
@@ -10,7 +10,7 @@ void shearlet_example()
     IndexVector_t size(2);
     size << 256, 256;
     auto phantom = PhantomGenerator<real_t>::createModifiedSheppLogan(size);
-    EDF::write(phantom, "128phantom.edf");
+    EDF::write(phantom, "256phantom.edf");
 
     ShearletTransform<real_t> shearletTransform(size);
 
