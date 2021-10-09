7afc8530a289fc179c55e4a556afd92490951bf9
andibraimllari <andibraimllari@gmail.com>
2021-10-10 01:17:41 +0200

    add more tests to the AutoEncoder, refactor

--- elsa/ml/tests/test_AutoEncoder.cpp
+++ elsa/ml/tests/test_AutoEncoder.cpp
@@ -18,7 +18,7 @@ using namespace doctest;
 
 TEST_SUITE_BEGIN("ml");
 
-TEST_CASE_TEMPLATE("AutoEncoder: Pre-training testing", TestType, float, double)
+TEST_CASE_TEMPLATE("AutoEncoder: pre-training testing", TestType, float, double)
 {
     GIVEN("a DataDescriptor")
     {
@@ -27,7 +27,65 @@ TEST_CASE_TEMPLATE("AutoEncoder: Pre-training testing", TestType, float, double)
 
         WHEN("instantiating an AutoEncoder model")
         {
+            THEN("no error is thrown")
+            {
+                REQUIRE_NOTHROW(
+                    ml::AutoEncoder<TestType, elsa::ml::MlBackend::Cudnn>{mnistShape, batchSize});
+            }
+        }
+
+        WHEN("instantiating an AutoEncoder model")
+        {
             ml::AutoEncoder<TestType, elsa::ml::MlBackend::Cudnn> aeModel(mnistShape, batchSize);
+
+            THEN("the shape of the input matches the shape of the output")
+            {
+                DataContainer<TestType> randomValues(mnistShape);
+                DataContainer<TestType> prediction = aeModel.predict(randomValues);
+                REQUIRE_EQ(prediction.getDataDescriptor(), mnistShape);
+            }
+        }
+    }
+}
+
+TEST_CASE_TEMPLATE("AutoEncoder: testing of training", TestType, float, double)
+{
+    GIVEN("a DataDescriptor")
+    {
+        VolumeDescriptor mnistShape({28, 28});
+        index_t batchSize = 32;
+
+        std::vector<DataContainer<TestType>> inputs;
+        // TODO perhaps generate one phantom image and augment it
+        std::vector<DataContainer<TestType>> labels;
+        // TODO perhaps generate one phantom image
+
+        WHEN("instantiating and training an AutoEncoder model")
+        {
+            ml::AutoEncoder<TestType, elsa::ml::MlBackend::Cudnn> aeModel(mnistShape, batchSize);
+
+            // define an Adam optimizer
+            auto optimizer = ml::Adam<TestType>();
+
+            // compile the model
+            aeModel.compile(ml::SparseCategoricalCrossentropy<TestType>(), &optimizer);
+
+            ml::Model<TestType, ml::MlBackend::Cudnn>::History history =
+                aeModel.fit(inputs, labels, 2);
+
+            THEN("all losses are non-NaNs")
+            {
+                std::vector<TestType> losses = history.loss;
+                for (auto loss : losses) {
+                    REQUIRE_UNARY(!std::isnan(loss));
+                }
+            }
+
+            THEN("no exception is thrown when predicting")
+            {
+                DataContainer<TestType> randomValues(mnistShape);
+                REQUIRE_NOTHROW(aeModel.predict(randomValues));
+            }
 
             THEN("the shape of the input matches the shape of the output")
             {
