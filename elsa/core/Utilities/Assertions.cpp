32cd20e146bbd5ac7afe615b8ecc1785eee383fa
Jonas Jelten <jj@sft.lol>
2022-11-02 19:16:17 +0100

    feat: introduce ENSURE macro
    can be extended to throw backtrace-capturing exceptions then.

--- elsa/core/Utilities/Assertions.cpp
+++ elsa/core/Utilities/Assertions.cpp
@@ -1,22 +1,16 @@
 #include "Assertions.h"
+#include "elsaDefines.h"
 
 namespace elsa::detail
 {
-    void assert_nomsg(const char* expr_str, bool expr, const char* file, int line)
-    {
-        if (!expr) {
-            std::cerr << "Assert failed:\n"
-                      << "Expected:\t" << expr_str << "\n"
-                      << "Source:\t\t" << file << ", line " << line << "\n";
-            abort();
-        }
-    }
-
     void assert_msg(const char* expr_str, bool expr, const char* file, int line, const char* msg)
     {
-        if (!expr) {
-            std::cerr << "Assert failed:\t" << msg << "\n"
-                      << "Expected:\t" << expr_str << "\n"
+        if (unlikely(not expr)) {
+            std::cerr << "Assert failed:";
+            if (msg != nullptr) {
+                std::cerr << "\t" << msg;
+            }
+            std::cerr << "\nExpected:\t" << expr_str << "\n"
                       << "Source:\t\t" << file << ", line " << line << "\n";
             abort();
         }
