69883adc1b7d58a1ca1f858afab255824dfbb231
David Alkier <d.alkier@tum.de>
2022-12-12 14:53:54 +0100

    feat: impl (Ellip)CylinderFree rasterization
    EllipCylinderFree are rasterized going through the bounding box.
    CylinderFree is a wrapper around EllipCylinderFree with the
    radius for both half axis.

--- elsa/generators/Ellipsoid.cpp
+++ elsa/generators/Ellipsoid.cpp
@@ -21,30 +21,7 @@ namespace elsa::phantoms
                   > 0;
 
         if (rotated) {
-            // convert to radians
-            auto phiRad = eulers[INDEX_PHI] * pi<double> / 180.0;
-            auto thetaRad = eulers[INDEX_THETA] * pi<double> / 180.0;
-            auto psiRad = eulers[INDEX_PSI] * pi<double> / 180.0;
-
-            auto cosPhi = std::cos(phiRad);
-            auto sinPhi = std::sin(phiRad);
-            auto cosTheta = std::cos(thetaRad);
-            auto sinTheta = std::sin(thetaRad);
-            auto cosPsi = std::cos(psiRad);
-            auto sinPsi = std::sin(psiRad);
-
-            rot(0, 0) = static_cast<real_t>(cosPhi * cosPsi - cosTheta * sinPhi * sinPsi);
-            rot(0, 1) = static_cast<real_t>(cosPsi * sinPhi + cosPhi * cosTheta * sinPsi);
-            rot(0, 2) = static_cast<real_t>(sinTheta * sinPsi);
-
-            rot(1, 0) = static_cast<real_t>(-cosPhi * sinPsi - cosTheta * cosPsi * sinPhi);
-            rot(1, 1) = static_cast<real_t>(cosPhi * cosTheta * cosPsi - sinPhi * sinPsi);
-            rot(1, 2) = static_cast<real_t>(cosPsi * sinTheta);
-
-            rot(2, 0) = static_cast<real_t>(sinPhi * sinTheta);
-            rot(2, 1) = static_cast<real_t>(-cosPhi * sinTheta);
-            rot(2, 2) = static_cast<real_t>(cosTheta);
-
+            fillRotationMatrix(eulers, rot);
             rot.transposeInPlace();
         }
     };
---
+++ elsa/generators/tests/test_CylinderFree.cpp
@@ -0,0 +1,69 @@
+/**
+ * @file CylinderFree.cpp
+ *
+ * @brief Test for CylinderFree
+ *
+ */
+
+#include "doctest/doctest.h"
+
+#include "CylinderFree.h"
+
+using namespace elsa;
+using namespace doctest;
+
+TEST_CASE("CylinderFree tests")
+{
+    GIVEN("A rotated CylinderFree ")
+    {
+        index_t size = 10;
+        IndexVector_t sizes(3);
+        sizes << size, size, size;
+
+        phantoms::Vec3i center;
+        center << 5.0, 5.0, 5.0;
+
+        double radius = 1.0;
+
+        double length{8.};
+        double amplit{1.};
+
+        phantoms::Vec3X<double> eulers;
+        eulers << 0.0, 45.0, 0.0;
+
+        VolumeDescriptor dd(sizes);
+        DataContainer<double> dc(dd);
+        dc = 0;
+
+        WHEN("Rasterize")
+        {
+
+            phantoms::CylinderFree<double> ec{amplit, center, radius, length, eulers};
+
+            phantoms::rasterize<double>(ec, dd, dc);
+
+            IndexVector_t idx(3);
+
+            WHEN("Check empty volume")
+            {
+                idx << 5, 5, 9;
+                REQUIRE_EQ(dc(idx), 0);
+
+                idx << 5, 5, 2;
+                REQUIRE_EQ(dc(idx), 0);
+            }
+            WHEN("Check filled volume")
+            {
+
+                idx << 5, 5, 5;
+                REQUIRE_EQ(dc(idx), amplit);
+
+                idx << 5, 6, 6;
+                REQUIRE_EQ(dc(idx), amplit);
+
+                idx << 5, 4, 4;
+                REQUIRE_EQ(dc(idx), amplit);
+            }
+        }
+    }
+}
