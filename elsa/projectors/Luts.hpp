e715d8fa36f162ea5f0cff4c741e2f890a62b159
Noah Dormann <n.dormann@tum.de>
2022-12-30 18:10:30 +0100

    refactor: reduce code duplication and simplify lerp

--- elsa/projectors/Luts.hpp
+++ elsa/projectors/Luts.hpp
@@ -12,17 +12,18 @@ namespace elsa
     namespace detail
     {
         template <typename data_t, index_t N>
-        constexpr std::array<data_t, N> blob_lut(ProjectedBlob<data_t> blob)
+        constexpr std::array<data_t, N + 1> generate_lut(ProjectedBlob<data_t> blob,
+                                                         std::function<data_t(data_t)> gen)
         {
-            Logger::get("blob_lut")->debug("Calculating lut");
+            Logger::get("generate_lut")->debug("Calculating lut");
 
-            std::array<data_t, N> lut;
+            std::array<data_t, N + 1> lut;
 
             auto t = static_cast<data_t>(0);
             const auto step = blob.radius() / N;
 
-            for (std::size_t i = 0; i < N; ++i) {
-                lut[i] = blob(t);
+            for (std::size_t i = 0; i <= N; ++i) {
+                lut[i] = gen(t);
                 t += step;
             }
 
@@ -30,52 +31,16 @@ namespace elsa
         }
 
         template <typename data_t, index_t N>
-        constexpr std::array<data_t, N> blob_derivative_lut(ProjectedBlob<data_t> blob)
-        {
-            Logger::get("blob_derivative_lut")->debug("Calculating lut");
-
-            std::array<data_t, N> lut;
-
-            auto t = static_cast<data_t>(0);
-            const auto step = blob.radius() / N;
-
-            for (std::size_t i = 0; i < N; ++i) {
-                lut[i] = blob.derivative(t);
-                t += step;
-            }
-
-            return lut;
-        }
-
-        template <typename data_t, index_t N>
-        constexpr std::array<data_t, N> blob_gradient_helper_lut(ProjectedBlob<data_t> blob)
-        {
-            Logger::get("blob_gradient_lut")->debug("Calculating lut");
-
-            std::array<data_t, N> lut;
-
-            auto t = static_cast<data_t>(0);
-            const auto step = blob.radius() / N;
-
-            for (std::size_t i = 0; i < N; ++i) {
-                lut[i] = blob.gradient_helper(t);
-                t += step;
-            }
-
-            return lut;
-        }
-
-        template <typename data_t, index_t N>
-        constexpr std::array<data_t, N> bspline_lut(ProjectedBSpline<data_t> bspline)
+        constexpr std::array<data_t, N + 1> bspline_lut(ProjectedBSpline<data_t> bspline)
         {
             Logger::get("bspline_lut")->debug("Calculating lut");
 
-            std::array<data_t, N> lut;
+            std::array<data_t, N + 1> lut;
 
             auto t = static_cast<data_t>(0);
             const auto step = 2. / N;
 
-            for (std::size_t i = 0; i < N; ++i) {
+            for (std::size_t i = 0; i <= N; ++i) {
                 lut[i] = bspline(t);
                 t += step;
             }
@@ -105,7 +70,7 @@ namespace elsa
     class Lut
     {
     public:
-        constexpr Lut(std::array<data_t, N> data) : data_(std::move(data)) {}
+        constexpr Lut(std::array<data_t, N + 1> data) : data_(std::move(data)) {}
 
         template <typename T, std::enable_if_t<std::is_integral_v<T>, int> = 0>
         constexpr data_t operator()(T index) const
@@ -123,6 +88,7 @@ namespace elsa
         template <typename T, std::enable_if_t<std::is_floating_point_v<T>, int> = 0>
         constexpr data_t operator()(T index) const
         {
+            // our functions are always zero at the end everything outside is 0
             if (index < 0 || index > N) {
                 return 0;
             }
@@ -132,22 +98,22 @@ namespace elsa
             const auto b = static_cast<std::size_t>(std::ceil(index));
 
             // Get the function values
-            const auto fa = a == N ? 0 : data_[a];
-            const auto fb = b == N ? 0 : data_[b];
+            const auto fa = data_[a];
+            const auto fb = data_[b];
 
-            auto ret = detail::lerp(fa, fb, index - static_cast<data_t>(a));
-
-            // Bilinear interpolation
-            return detail::lerp(fa, fb, index - static_cast<data_t>(a));
+            const auto t = index - static_cast<data_t>(a);
+            return t * fb + (1 - t) * fa;
         }
 
+        constexpr auto data() const { return data_; }
+
     private:
-        std::array<data_t, N> data_;
+        std::array<data_t, N + 1> data_;
     };
 
     // User defined deduction guide
     template <typename data_t, std::size_t N>
-    Lut(std::array<data_t, N>) -> Lut<data_t, N>;
+    Lut(std::array<data_t, N>) -> Lut<data_t, N - 1>;
 
     template <typename data_t, index_t N>
     class ProjectedBlobLut
@@ -155,7 +121,8 @@ namespace elsa
     public:
         constexpr ProjectedBlobLut(data_t radius, SelfType_t<data_t> alpha,
                                    SelfType_t<data_t> order)
-            : blob_(radius, alpha, order), lut_(detail::blob_lut<data_t, N>(blob_))
+            : blob_(radius, alpha, order),
+              lut_(detail::generate_lut<data_t, N>(blob_, [this](data_t s) { return blob_(s); }))
         {
         }
 
@@ -170,6 +137,8 @@ namespace elsa
             return lut_((distance / blob_.radius()) * N);
         }
 
+        constexpr auto data() const { return lut_.data(); }
+
     private:
         ProjectedBlob<data_t> blob_;
         Lut<data_t, N> lut_;
@@ -181,7 +150,9 @@ namespace elsa
     public:
         constexpr ProjectedBlobDerivativeLut(data_t radius, SelfType_t<data_t> alpha,
                                              SelfType_t<data_t> order)
-            : blob_(radius, alpha, order), lut_(detail::blob_derivative_lut<data_t, N>(blob_))
+            : blob_(radius, alpha, order),
+              lut_(detail::generate_lut<data_t, N>(
+                  blob_, [this](data_t s) { return blob_.derivative(s); }))
         {
         }
 
@@ -207,7 +178,9 @@ namespace elsa
     public:
         constexpr ProjectedBlobGradientHelperLut(data_t radius, SelfType_t<data_t> alpha,
                                                  SelfType_t<data_t> order)
-            : blob_(radius, alpha, order), lut_(detail::blob_gradient_helper_lut<data_t, N>(blob_))
+            : blob_(radius, alpha, order),
+              lut_(detail::generate_lut<data_t, N>(
+                  blob_, [this](data_t s) { return blob_.gradient_helper(s); }))
         {
         }
 
