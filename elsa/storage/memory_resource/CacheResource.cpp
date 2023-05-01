0e5506ca8a270c958dc95c7c212affb3374ca041
Fabian Stemmler <stemmler@in.tum.de>
2023-05-01 21:07:46 +0200

    implement cache resource

---
+++ elsa/storage/memory_resource/CacheResource.cpp
@@ -0,0 +1,110 @@
+#include "CacheResource.h"
+#include <limits>
+
+namespace elsa::mr
+{
+    CacheResourceConfig::CacheResourceConfig(size_t maxCacheSize, size_t maxCachedCount)
+        : maxCacheSize{maxCacheSize}, maxCachedCount{maxCachedCount}
+    {
+    }
+
+    CacheResourceConfig CacheResourceConfig::defaultConfig()
+    {
+        return CacheResourceConfig(std::numeric_limits<size_t>::max(), 16);
+    }
+
+    CacheResourceConfig& CacheResourceConfig::setMaxCacheSize(size_t size)
+    {
+        maxCacheSize = size;
+        return *this;
+    }
+
+    CacheResourceConfig& CacheResourceConfig::setMaxCachedCount(size_t count)
+    {
+        maxCachedCount = count;
+        return *this;
+    }
+
+    void CacheResource::releaseCache()
+    {
+        for (auto& entry : _cache) {
+            _upstream->deallocate(entry.ptr, entry.size, entry.alignment);
+        }
+        _cache.clear();
+        _sizeToCacheElement.clear();
+    }
+
+    CacheResource::CacheResource(MemoryResource upstream, const CacheResourceConfig& config)
+        : _upstream{upstream}, _config{config}, _sizeToCacheElement{config.maxCachedCount}
+    {
+    }
+
+    MemoryResource CacheResource::make(MemoryResource upstream, const CacheResourceConfig& config)
+    {
+        return MemoryResource::MakeRef(new CacheResource(upstream, config));
+    }
+
+    void* CacheResource::allocate(size_t size, size_t alignment)
+    {
+        auto mapIt = _sizeToCacheElement.find({size, alignment});
+        if (mapIt == _sizeToCacheElement.end()) {
+            try {
+                return _upstream->allocate(size, alignment);
+            } catch (std::bad_alloc& e) {
+                releaseCache();
+                // try again after hopefully returning enough memory to the upstream allocator
+                return _upstream->allocate(size, alignment);
+            }
+        } else {
+            _sizeToCacheElement.erase(mapIt);
+            void* ptr = mapIt->second->ptr;
+            _cache.erase(mapIt->second);
+            return ptr;
+        }
+    }
+
+    void CacheResource::deallocate(void* ptr, size_t size, size_t alignment) noexcept
+    {
+        auto cacheIt = _cache.end();
+        try {
+            _cache.push_back({ptr, size, alignment});
+            cacheIt--;
+            _sizeToCacheElement.insert({{size, alignment}, --_cache.end()});
+        } catch (std::bad_alloc& e) {
+            _cache.erase(cacheIt);
+            _upstream->deallocate(ptr, size, alignment);
+            return;
+        }
+        _cachedSize += size;
+        if (_cache.size() >= _config.maxCachedCount || _cachedSize >= _config.maxCacheSize) {
+            cache_resource::CacheElement& poppedElement = _cache.front();
+            auto poppedIt = _sizeToCacheElement.find({poppedElement.size, poppedElement.alignment});
+            // TODO: exception if poppedElement is not found, i.e. the invariants are violated
+            while (&*poppedIt->second != &poppedElement)
+                poppedIt++;
+            _sizeToCacheElement.erase(poppedIt);
+            _cachedSize -= poppedElement.size;
+            _cache.pop_front();
+        }
+    }
+
+    bool CacheResource::tryResize(void* ptr, size_t size, size_t alignment, size_t newSize)
+    {
+        return _upstream->tryResize(ptr, size, alignment, newSize);
+    }
+
+    void CacheResource::copyMemory(void* ptr, const void* src, size_t size)
+    {
+        _upstream->copyMemory(ptr, src, size);
+    }
+
+    void CacheResource::setMemory(void* ptr, const void* src, size_t stride, size_t count)
+    {
+        _upstream->setMemory(ptr, src, stride, count);
+    }
+
+    void CacheResource::moveMemory(void* ptr, const void* src, size_t size)
+    {
+        _upstream->moveMemory(ptr, src, size);
+    }
+} // namespace elsa::mr
---
+++ elsa/storage/memory_resource/CacheResource.h
@@ -0,0 +1,88 @@
+#pragma once
+#include "ContiguousMemory.h"
+#include <unordered_map>
+#include <list>
+#include <memory>
+
+template <>
+class std::hash<std::pair<size_t, size_t>>
+{
+public:
+    size_t operator()(const std::pair<size_t, size_t>& pair) const
+    {
+        return std::hash<size_t>()(std::hash<size_t>()(pair.first) ^ pair.second);
+    }
+};
+
+namespace elsa::mr
+{
+    namespace cache_resource
+    {
+        struct CacheElement {
+            void* ptr;
+            size_t size;
+            size_t alignment;
+        };
+    } // namespace cache_resource
+
+    class CacheResource;
+
+    class CacheResourceConfig
+    {
+    private:
+        friend class CacheResource;
+
+        size_t maxCacheSize;
+        size_t maxCachedCount;
+
+        CacheResourceConfig(size_t maxCacheSize, size_t maxCachedCount);
+
+    public:
+        /// @brief Default configuration for a cache resource with (hopefully) sensible defaults.
+        /// @return Default configuration for a cache resource.
+        static CacheResourceConfig defaultConfig();
+
+        /// @brief Set the maximum cumulative size of cached chunks, before releasing chunks to the
+        /// upstream allocator
+        /// @param size Maximum cumulative size of cached chunks
+        /// @return self
+        CacheResourceConfig& setMaxCacheSize(size_t size);
+
+        /// @brief Set the maximum number of cached chunks, before releasing chunks to the
+        /// upstream allocator
+        /// @param count Maximum number of cached chunks
+        /// @return self
+        CacheResourceConfig& setMaxCachedCount(size_t count);
+    };
+
+    class CacheResource : public MemResInterface
+    {
+    private:
+        using Cache = std::list<cache_resource::CacheElement>;
+        MemoryResource _upstream;
+        CacheResourceConfig _config;
+        std::unordered_multimap<std::pair<size_t, size_t>, Cache::iterator> _sizeToCacheElement;
+        Cache _cache;
+        size_t _cachedSize{0};
+
+        void releaseCache();
+
+    protected:
+        CacheResource(MemoryResource upstream,
+                      const CacheResourceConfig& config = CacheResourceConfig::defaultConfig());
+
+        ~CacheResource() = default;
+
+    public:
+        static MemoryResource
+            make(MemoryResource upstream,
+                 const CacheResourceConfig& config = CacheResourceConfig::defaultConfig());
+
+        void* allocate(size_t size, size_t alignment) override;
+        void deallocate(void* ptr, size_t size, size_t alignment) noexcept override;
+        bool tryResize(void* ptr, size_t size, size_t alignment, size_t newSize) override;
+        void copyMemory(void* ptr, const void* src, size_t size) override;
+        void setMemory(void* ptr, const void* src, size_t stride, size_t count) override;
+        void moveMemory(void* ptr, const void* src, size_t size) override;
+    };
+} // namespace elsa::mr
