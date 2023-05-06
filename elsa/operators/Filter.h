abd6dded1da37a538a834b2178fc8f36bbb0b388
Cederik Höfs <cederik_hoefs@t-online.de>
2023-05-07 00:10:19 +0200

    use data_t

--- elsa/operators/Filter.h
+++ elsa/operators/Filter.h
@@ -37,7 +37,7 @@ namespace elsa
         auto sliceDesc = VolumeDescriptor{coefficients};
         auto midPoint = coefficients / 2;
 
-        DataContainer<complex_t> filter{sliceDesc};
+        DataContainer<add_complex_t<data_t>> filter{sliceDesc};
 
         if (!(dim == 2 || dim == 3)) {
             throw InvalidArgumentError("makeFilter:: Should only be used for 2D or 3D sinograms");
@@ -70,8 +70,8 @@ namespace elsa
     Filter<data_t> makeRamLak(const DataDescriptor& descriptor)
     {
         auto n = descriptor.getNumberOfCoefficientsPerDimension()[0] / 2;
-        auto kmax = kMax(descriptor);
-        return makeFilter(descriptor, [&](IndexVector_t nu) {
+        auto kmax = kMax<data_t>(descriptor);
+        return makeFilter<data_t>(descriptor, [&](IndexVector_t nu) {
             auto deltak = 2 * pi_t / n; // TODO: DC bin might have different factor in 3d
             auto k = 2 * pi_t * nu.norm();
             return (nu.isZero() ? 0.25 * deltak : k) / kmax;
@@ -81,8 +81,8 @@ namespace elsa
     template <typename data_t = float>
     Filter<data_t> makeSheppLogan(const DataDescriptor& descriptor)
     {
-        auto kmax = kMax(descriptor);
-        return makeFilter(descriptor, [&](IndexVector_t nu) {
+        auto kmax = kMax<data_t>(descriptor);
+        return makeFilter<data_t>(descriptor, [&](IndexVector_t nu) {
             auto k = 2 * pi_t * nu.norm();
             return std::sin(pi_t / 2 * k / kmax) * 2
                    / pi_t; // TODO: Look up definition of sinc (factor 1/pi?)
@@ -92,8 +92,8 @@ namespace elsa
     template <typename data_t = float>
     Filter<data_t> makeCosine(const DataDescriptor& descriptor)
     {
-        auto kmax = kMax(descriptor);
-        return makeFilter(descriptor, [&](IndexVector_t nu) {
+        auto kmax = kMax<data_t>(descriptor);
+        return makeFilter<data_t>(descriptor, [&](IndexVector_t nu) {
             auto k = 2 * pi_t * nu.norm();
             return std::sin(pi_t * k / kmax) / pi_t;
         });
@@ -102,8 +102,8 @@ namespace elsa
     template <typename data_t = float>
     Filter<data_t> makeHann(const DataDescriptor& descriptor)
     {
-        auto kmax = kMax(descriptor);
-        return makeFilter(descriptor, [&](IndexVector_t nu) {
+        auto kmax = kMax<data_t>(descriptor);
+        return makeFilter<data_t>(descriptor, [&](IndexVector_t nu) {
             auto k = 2 * pi_t * nu.norm();
             return k / kmax * std::cos(pi_t / 2 * k / kmax) * std::cos(pi_t / 2 * k / kmax);
         });
