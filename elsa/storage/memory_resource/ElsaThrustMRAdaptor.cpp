fb567e0da4281c0e9e75f64940b30a25e3c508fd
Fabian Stemmler <stemmler@in.tum.de>
2023-05-24 10:44:51 +0200

    implement elsa/thrust memory resource adaptors

---
+++ elsa/storage/memory_resource/ElsaThrustMRAdaptor.cpp
@@ -0,0 +1,19 @@
+#include "ElsaThrustMRAdaptor.h"
+
+elsa::mr::ElsaThrustMRAdaptor::pointer elsa::mr::ElsaThrustMRAdaptor::do_allocate(std::size_t bytes,
+                                                                          std::size_t alignment)
+{
+    return pointer(_mr->allocate(bytes, alignment));
+}
+
+void elsa::mr::ElsaThrustMRAdaptor::do_deallocate(pointer p, std::size_t bytes, std::size_t alignment)
+{
+    _mr->deallocate(p.get(), bytes, alignment);
+}
+
+bool elsa::mr::ElsaThrustMRAdaptor::do_is_equal(const memory_resource& other) const noexcept
+{
+    return this == &other;
+}
+
+elsa::mr::ElsaThrustMRAdaptor::ElsaThrustMRAdaptor() : _mr{elsa::mr::defaultInstance()} {}
---
+++ elsa/storage/memory_resource/ThrustElsaMRAdaptor.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "ContiguousMemory.h"
+
+namespace elsa::mr {
+    template<typename T>
+    class ThrustElsaMRAdaptor : public MemResInterface {
+    private:
+        T _thrustMR;
+
+    protected:
+        template <typename... Ts>
+        ThrustElsaMRAdaptor(Ts... args) : _thrustMR{args...} {};
+    public:        
+        void * allocate(size_t size, size_t alignment) override {
+            return _thrustMR.do_allocate(size, alignment).get();
+        };
+        void deallocate(void *ptr, size_t size, size_t alignment) noexcept override {
+            return _thrustMR.do_deallocate((typename T::pointer)(ptr), size, alignment);
+        }
+        bool tryResize(void *ptr, size_t size, size_t alignment, size_t newSize) override {
+            return false;
+        }
+
+        template <typename... Ts>
+        static MemoryResource make(Ts... args) {
+            return MemoryResource::MakeRef(new ThrustElsaMRAdaptor<T>(args...));
+        };
+    };
+}
