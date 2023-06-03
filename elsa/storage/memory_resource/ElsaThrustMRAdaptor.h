169b913f4a48fd99c98f34beb29583db2cae9935
Fabian Stemmler <stemmler@in.tum.de>
2023-06-03 14:19:53 +0200

    document memory resource adaptors

--- elsa/storage/memory_resource/ElsaThrustMRAdaptor.h
+++ elsa/storage/memory_resource/ElsaThrustMRAdaptor.h
@@ -5,6 +5,16 @@
 
 namespace elsa::mr
 {
+    /// @brief Wraps an elsa::mr::MemoryResource into a thrust::mr::memory_resource for use with
+    /// e.g. thrust::universal_vector.
+    ///
+    /// thrust::mr::memory_resource::do_allocate -> elsa::mr::MemResInterface::allocate
+    ///
+    /// thrust::mr::memory_resource::do_deallocate -> elsa::mr::MemResInterface::deallocate
+    ///
+    /// thrust::mr::memory_resource::do_is_equal -> basic address comparison with other,
+    /// cannot be mapped directly to elsa::mr::MemoryResource::operator==, because it must be
+    /// tagged as __host__ __device__
     class ElsaThrustMRAdaptor final
         : public thrust::mr::memory_resource<thrust::universal_ptr<void>>
     {
@@ -13,8 +23,11 @@ namespace elsa::mr
 
     public:
         using thrust::mr::memory_resource<thrust::universal_ptr<void>>::pointer;
+        /// @brief maps to elsa::mr::MemResInterface::allocate
         pointer do_allocate(std::size_t bytes, std::size_t alignment) override;
+        /// @brief maps to elsa::mr::MemResInterface::deallocate
         void do_deallocate(pointer p, std::size_t bytes, std::size_t alignment) override;
+        /// @return true iff. this == &other
         __host__ __device__ bool do_is_equal(const memory_resource& other) const noexcept override;
 
     public:
--- elsa/storage/memory_resource/ThrustElsaMRAdaptor.h
+++ elsa/storage/memory_resource/ThrustElsaMRAdaptor.h
@@ -3,6 +3,14 @@
 
 namespace elsa::mr
 {
+    /// @brief Wraps a thrust::mr::memory_resource into the elsa::mr::MemResInterface for use with
+    /// e.g. elsa::mr::ContiguousVector
+    /// @tparam T type of the wrapped thrust resource
+    /// elsa::mr::MemResInterface::allocate -> thrust::mr::memory_resource::do_allocate
+    ///
+    /// elsa::mr::MemResInterface::deallocate -> thrust::mr::memory_resource::do_deallocate
+    ///
+    /// elsa::mr::MemResInterface::tryResize -> false
     template <typename T>
     class ThrustElsaMRAdaptor : public MemResInterface
     {
@@ -14,14 +22,18 @@ namespace elsa::mr
         ThrustElsaMRAdaptor(Ts... args) : _thrustMR{args...} {};
 
     public:
+        /// @brief maps to thrust::mr::memory_resource::do_allocate
         void* allocate(size_t size, size_t alignment) override
         {
             return _thrustMR.do_allocate(size, alignment).get();
         };
+        /// @brief maps to thrust::mr::memory_resource::do_deallocate
         void deallocate(void* ptr, size_t size, size_t alignment) noexcept override
         {
             return _thrustMR.do_deallocate((typename T::pointer)(ptr), size, alignment);
         }
+        /// @brief This is a no-op.
+        /// @return false
         bool tryResize(void* ptr, size_t size, size_t alignment, size_t newSize) override
         {
             return false;
